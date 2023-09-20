361bf81617ec89cffe78297239083b94143e7e2e
Sukrija Kararic <sukrijakararic1@gmail.com>
2023-09-20 08:43:05 -0400

    added dark mode and consolidated pages

---
+++ script.js
@@ -0,0 +1,11 @@
+const dark = document.getElementById("dark-mode")
+
+function darkmode () {
+    const isItDark = document.body.classList.contains('dark-mode');
+
+    if (!isItDark) {
+        return document.body.classList.add('dark-mode');
+    } document.body.classList.remove('dark-mode');
+};
+
+dark.onclick = darkmode
